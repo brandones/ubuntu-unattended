a6755ee5010aeb73d175fad388c28e26de31a49d
Brandon Istenes <bistenes@gmail.com>
2018-07-26 12:02:49 -0500

    Remove Desktop support -- doesn't work

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -86,7 +86,6 @@ while true; do
     echo "  [2] Ubuntu $trus LTS Server amd64 - Trusty Tahr"
     echo "  [3] Ubuntu $xenn LTS Server amd64 - Xenial Xerus"
     echo "  [4] Ubuntu $bion LTS Server amd64 - Bionic Beaver"
-    echo "  [5] Ubuntu $xenn LTS Desktop amd64 - Xenial Xerus"
     echo
     read -p " please enter your preference: [1|2|3|4]: " ubver
     case $ubver in
@@ -106,11 +105,7 @@ while true; do
                 download_location="http://cdimage.ubuntu.com/releases/$bion/release/"
                 new_iso_name="ubuntu-$bion-server-amd64-unattended.iso"
                 break;;
-        [5]* )  download_file="ubuntu-$xenn-desktop-amd64.iso"
-                download_location="http://releases.ubuntu.com/$xenn/"
-                new_iso_name="ubuntu-$xenn-desktop-amd64-unattended.iso"
-                break;;
-        * ) echo " please answer [1], [2], [3], [4], or [5]";;
+        * ) echo " please answer [1], [2], [3] or [4]";;
     esac
 done
 
